467513610870e6748d48559043f8d2259da4d44f
Daniel Alexander <daniel.gt@tecjerez.edu.mx>
2025-05-21 08:34:22 -0600

    adicion de la estructura de los hilos

---
+++ src/Hilos/HilosConsultaActualizarGUI.java
@@ -0,0 +1,35 @@
+package Hilos;
+
+import java.sql.ResultSet;
+
+import javax.swing.SwingUtilities;
+
+import ConexionBD.ConexionBD;
+import Vista.VentanaInicio;
+
+public class HilosConsultaActualizarGUI {
+    private final VentanaInicio interfaz;
+    private String sql;
+
+    public HilosConsultaActualizarGUI(VentanaInicio interfaz, String instruccionSQL) {
+        this.interfaz = interfaz;
+       this.sql = instruccionSQL;
+    }
+
+    public void consultarYActualizarGUI() {
+        new Thread(() -> {								// Hilo que hace la consulta 
+            try {
+                // Obtener datos desde la base de datos
+                ResultSet resultado = ConexionBD.getInstancia().ejecutarInstruccionSQL(sql);
+                // Actualizar GUI en el hilo de eventos de Swing
+                SwingUtilities.invokeLater(() -> {
+                    interfaz.actualizarConDatos(resultado);
+                });
+
+            } catch (Exception e) {
+                System.err.println("Error al consultar datos: " + e.getMessage());
+            }
+        }).start();
+    }
+}
+}
